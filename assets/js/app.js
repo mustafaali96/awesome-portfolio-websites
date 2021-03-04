bb2ad798457c8263c1d608848c7d83db52ffea90
mustafaali96 <ssuet.se2016@gmail.com>
2021-03-04 18:15:16 +0500

    name

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,6 +18,7 @@ let header = $(`
    <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
    <li class="nav-item"><a class="nav-link" href="experience.html">Experience</a></li>
    <li class="nav-item"><a class="nav-link" href="projects.html">Projects</a></li>
+   <li class="nav-item"><a class="nav-link" href="certification.html">Certification</a></li>
    <li class="nav-item"><a class="nav-link" href="research.html">Research</a></li>
    <li class="nav-item"><a class="nav-link" href="education.html">Education</a></li>
    <div class="bike">
