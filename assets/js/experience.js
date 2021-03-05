6ef0b5915a4b9ee34df85b6a91d014b879ccdca2
mustafaali96 <ssuet.se2016@gmail.com>
2021-03-05 18:15:00 +0500

    experience page

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -4,25 +4,25 @@
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
-    cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    title: "Python Developer",
+    cardImage: "assets/images/experience-page/ast.jpg",
+    place: "All Star Technology",
+    time: "(Dec, 2020 - present)",
+    desp: "<li>Developed Machine Learning model for Project Management Tool to predict resources for upcoming projects.</li> <li>Integrate applications with third-party web services.</li> <li>Created API endpoint to send/receive data thru web server.</li>",
   },
   {
-    title: "Student Developer",
-    cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
-    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
+    title: "Computer Vision Engineer",
+    cardImage: "assets/images/experience-page/xclusive-solutions.jpg",
+    place: "Xclusive Solutions",
+    time: "(Feb - Dec, 2020)",
+    desp: "<li>Developed Artificial Intelligence based Automatic License Plate Recognition system using Darknet and pytesseract which detect traffic violation.</li> <li>Tested toll collection ALPR system on single network at FWO Karachi highway</li> <li>Automate Tolling system which maintains the record of driver's enter/exit time.</li>",
   },
   {
-    title: "Research Intern",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp:"<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+    title: "Computer Vision Intern/Teaching Assistant",
+    cardImage: "assets/images/experience-page/bleed-ai.png",
+    place: "Bleed AI",
+    time: "(Nov, 18 - Dec, 19)",
+    desp:"<li>Provide assistance on 2 months Traditional Computer Vision Course offered at Sir Syed University of Engineering and Technology.</li><li>Hands-on practice on different Feature Extraction Algorithms like HOG, SIFT, SURF, and BRIEF</li>",
   },
 ];
 
