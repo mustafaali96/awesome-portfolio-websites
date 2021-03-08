33b2cd20cdb4efb904d344031306f47c54908fa6
mustafaali96 <ssuet.se2016@gmail.com>
2021-03-08 16:56:58 +0500

    certification page

--- assets/js/certification.js
+++ assets/js/certification.js
@@ -6,81 +6,81 @@ const moocs = document.querySelector(".moocs");
 const moocscards = [
   {
     title: "Data Science",
-    cardImage: "assets/images/education-page/coursera1.png",
+    cardImage: "assets/images/certification-page/coursera1.png",
     moocLink: "https://www.coursera.org/browse/data-science",
   },
   {
     title: "Cryptography",
-    cardImage: "assets/images/education-page/udemy.png",
+    cardImage: "assets/images/certification-page/udemy.png",
     moocLink: "https://www.udemy.com/course/cryptography-for-beginners/",
   },
   {
     title: "Machine Learning",
-    cardImage: "assets/images/education-page/coursera1.png",
+    cardImage: "assets/images/certification-page/coursera1.png",
     moocLink: "https://www.coursera.org/learn/machine-learning",
   },
   {
     title: "Introduction to HTML 5",
-    cardImage: "assets/images/education-page/udacity_logo.png",
+    cardImage: "assets/images/certification-page/udacity_logo.png",
     moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
   },
   {
     title: "Introduction to CSS 3",
-    cardImage: "assets/images/education-page/udacity_logo.png",
+    cardImage: "assets/images/certification-page/udacity_logo.png",
     moocLink: "https://www.udacity.com/course/intro-to-html-and-css--ud001",
   },
   {
     title: "Javascript",
-    cardImage: "assets/images/education-page/udacity_logo.png",
+    cardImage: "assets/images/certification-page/udacity_logo.png",
     moocLink: "https://www.udacity.com/course/intro-to-javascript--ud803",
   },
   {
     title: "Bootstrap 4",
-    cardImage: "assets/images/education-page/udemy.png",
+    cardImage: "assets/images/certification-page/udemy.png",
     moocLink: "https://www.udemy.com/course/bootstrap-4-tutorials/",
   },
   {
     title: "Intro to React",
-    cardImage: "assets/images/education-page/EdX.png",
+    cardImage: "assets/images/certification-page/EdX.png",
     moocLink: "https://www.edx.org/learn/reactjs",
   },
   {
     title: "Intro to React Native",
-    cardImage: "assets/images/education-page/coursera1.png",
+    cardImage: "assets/images/certification-page/coursera1.png",
     moocLink: "https://www.coursera.org/lecture/react-native/introduction-to-react-native-Eax0D",
   },
   {
     title: "NodeJS, Express and MongoDB",
-    cardImage: "assets/images/education-page/coursera1.png",
+    cardImage: "assets/images/certification-page/coursera1.png",
     moocLink: "https://www.coursera.org/learn/server-side-nodejs",
   },
   {
     title: "XML-AJAX",
-    cardImage: "assets/images/education-page/udemy.png",
+    cardImage: "assets/images/certification-page/udemy.png",
     moocLink: "https://www.udemy.com/course/xml-from-beginner-to-expert/",
   },
   {
     title: "Data Structures & Algorithms",
-    cardImage: "assets/images/education-page/udacity_logo.png",
+    cardImage: "assets/images/certification-page/udacity_logo.png",
     moocLink: "https://www.udacity.com/course/data-structures-and-algorithms-nanodegree--nd256",
   },
 ];
 
 const experience = [
   {
-    img: "assets/images/education-page/c1.png"
+    img: "assets/images/certification-page/c1.png"
   },
   {
-    img: "assets/images/education-page/c2.jpg"
+    img: "assets/images/certification-page/c2.jpg"
   },
   {
-    img: "assets/images/education-page/c3.png"
+    img: "assets/images/certification-page/c3.png"
   },
   {
-    img: "assets/images/education-page/c4.png"
+    img: "assets/images/certification-page/c4.png"
   },
   {
-    img: "assets/images/education-page/c5.jpg"
+    img: "assets/images/certification-page/c5.jpg"
   },
 ];
 
@@ -140,17 +140,17 @@ const bagdes = document.querySelector(".badges");
 const badgesection = [
   {
     title: "Google Developer Essentials",
-    image: "assets/images/education-page/badge1.png",
+    image: "assets/images/certification-page/badge1.png",
     description: "Earned May 20, 2020",
   },
   {
     title: "VM Migration",
-    image: "assets/images/education-page/badge2.png",
+    image: "assets/images/certification-page/badge2.png",
     description: "Earned June 20, 2020",
   },
   {
     title: "G Suite Essentials",
-    image: "assets/images/education-page/badge3.png",
+    image: "assets/images/certification-page/badge3.png",
     description: "Earned July 20, 2020",
   },
 ];
@@ -177,28 +177,28 @@ const timeline = document.querySelector(".timeline");
 const timelinesection = [
   {
     heading: "Massachusetts Institute of Technology",
-    image: "./assets/images/education-page/timeline-1.svg",
+    image: "./assets/images/certification-page/timeline-1.svg",
     subheading: "Bachelors of Science, Computer Science & Engineering, (2020-2024)",
     description: "<li>Top 1% in the department</li><li>Best undergraduate Reseracher</li><li>Betsy Schumacker Woman</li><li>Athlete of the Year Award</li>",
   },
 
   {
     heading: "Massachusetts Institute of Technology",
-    image: "./assets/images/education-page/timeline-1.svg",
+    image: "./assets/images/certification-page/timeline-1.svg",
     subheading: "Bachelors of Science, Computer Science & Engineering, (2020-2024)",
     description: "<li>Top 1% in the department</li><li>Best undergraduate Reseracher</li><li>Betsy Schumacker Woman</li><li>Athlete of the Year Award</li>",
   },
 
   {
     heading: "Massachusetts Institute of Technology",
-    image: "./assets/images/education-page/timeline-1.svg",
+    image: "./assets/images/certification-page/timeline-1.svg",
     subheading: "Bachelors of Science, Computer Science & Engineering, (2020-2024)",
     description: "<li>Top 1% in the department</li><li>Best undergraduate Reseracher</li><li>Betsy Schumacker Woman</li><li>Athlete of the Year Award</li>",
   },
 
   {
     heading: "Massachusetts Institute of Technology",
-    image: "./assets/images/education-page/timeline-1.svg",
+    image: "./assets/images/certification-page/timeline-1.svg",
     subheading: "Bachelors of Science, Computer Science & Engineering, (2020-2024)",
     description: "<li>Top 1% in the department</li><li>Best undergraduate Reseracher</li><li>Betsy Schumacker Woman</li><li>Athlete of the Year Award</li>",
   },
@@ -209,20 +209,20 @@ const showCards2 = () => {
   timelinesection.forEach(
     ({ heading, image, subheading, description }) =>
     (output += `       
-      <div class="timeline" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="600">
+      <div class="timeline" data-aos="fade-up" data-aos-easing="linear" data-aos-delay="300">
       <div class="timeline-content">
-        <h6 class="center-align font-weight-bolder pt-1">${heading}</h6>
+        <h4 class="center-align font-weight-bolder pt-1">${heading}</h6>
         <div class="row">
           <div class="col-lg-4 col-md-4 col-sm-4">
             <img src="${image}" alt="" height="110"
               class=" mt-2 w-100 mx-auto">
           </div>
           <div class="col-lg-8 col-md-8 col-sm-8 px-4">
-            <span style="opacity: 0.9; font-size: 15px;">
+            <span style="opacity: 0.9; font-size: 20px;">
               ${subheading}
             </span>
-            <h6 class="text-muted mt-2" style="font-size: small;">Activities:</h6>
-            <ol style="margin-top: -8px; margin-left: -10px; font-size: 12px; list-style: disc;">
+            <h5 class="text-muted mt-2" style="font-size: small;">Activities:</h6>
+            <ol style="margin-top: -8px; margin-left: -10px; font-size: 15px; list-style: disc;">
               ${description}
             </ol>
           </div>
